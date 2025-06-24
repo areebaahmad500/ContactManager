5c3e84b61461628ee3299bbc8204b2cf27df7ec5
areebaahmad500 <17eareeba@gmail.com>
2025-06-25 01:09:31 +0500

    contact.java
    we update the code by adding getter setters

--- src/contact.java
+++ src/contact.java
@@ -10,16 +10,28 @@ public class contact {
         this.email = email;
     }
 
-    public String getName() { return name; }
-    public String getPhone() { return phone; }
-    public String getEmail() { return email; }
+    public void setName(String name) {
+        this.name = name;          
+ }
+        public void setPhone(String phone) {
+        	this.phone = phone;
+}
+public void setEmail(String email) {
+    this.email = email;
+}
+    public String getName() {
+        return name;
+    }
+    public String getPhone() {
+    	return phone;
+    	}
+    public String getEmail() {
+    	return email;
+    	}
 
-    public void setPhone(String phone) { this.phone = phone; }
-    public void setEmail(String email) { this.email = email; }
-
-    @Override
     public String toString() {
-        return name + " - " + phone + " - " + email;
+        return "Name: " + name + "\nPhone: " + phone + "\nEmail: " + email;
     }
 }
+
 
